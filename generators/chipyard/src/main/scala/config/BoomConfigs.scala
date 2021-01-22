29a8c6ec03d5d6a1c2c4d62495b747720fa0aedc
“lxu28973” <lxu28973@gmail.com>
2021-01-22 12:30:38 +0800

    add 6 wide BOOM config

--- generators/chipyard/src/main/scala/config/BoomConfigs.scala
+++ generators/chipyard/src/main/scala/config/BoomConfigs.scala
@@ -101,6 +101,25 @@ class MyMegaBoomConfig extends Config(
     new freechips.rocketchip.subsystem.WithCoherentBusTopology ++
     new freechips.rocketchip.system.BaseConfig)
 
+class My6WideBoomConfig extends Config(
+  new chipyard.iobinders.WithUARTAdapter ++
+    new chipyard.iobinders.WithTieOffInterrupts ++
+    new chipyard.iobinders.WithBlackBoxSimMem ++
+    new chipyard.iobinders.WithTiedOffDebug ++
+    new chipyard.iobinders.WithSimSerial ++
+    new testchipip.WithTSI ++
+    new chipyard.config.WithBootROM ++
+    new chipyard.config.WithUART ++
+    new chipyard.config.WithL2TLBs(1024) ++
+    new freechips.rocketchip.subsystem.WithNoMMIOPort ++
+    new freechips.rocketchip.subsystem.WithNoSlavePort ++
+    new freechips.rocketchip.subsystem.WithInclusiveCache ++
+    new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++
+    new boom.common.WithMy6WideBooms ++                              // my 6-wide boom config
+    new boom.common.WithNBoomCores(1) ++
+    new freechips.rocketchip.subsystem.WithCoherentBusTopology ++
+    new freechips.rocketchip.system.BaseConfig)
+
 class MySmallBoomConfig extends Config(
   new chipyard.iobinders.WithUARTAdapter ++                      // display UART with a SimUARTAdapter
     new chipyard.iobinders.WithTieOffInterrupts ++                 // tie off top-level interrupts
