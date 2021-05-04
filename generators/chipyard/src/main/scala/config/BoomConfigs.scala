b4d009435e4211f55f0f7025d93573e52e95f48f
“lxu28973” <lxu28973@gmail.com>
2021-05-04 18:44:12 +0800

    add MyGigaBoom

--- generators/chipyard/src/main/scala/config/BoomConfigs.scala
+++ generators/chipyard/src/main/scala/config/BoomConfigs.scala
@@ -172,7 +172,26 @@ class GigaBoomConfig extends Config(
     new freechips.rocketchip.subsystem.WithNoSlavePort ++
     new freechips.rocketchip.subsystem.WithInclusiveCache ++
     new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++
-    new boom.common.WithGigaBooms ++                              // my 4-wide boom config
+    new boom.common.WithGigaBooms ++
+    new boom.common.WithNBoomCores(1) ++
+    new freechips.rocketchip.subsystem.WithCoherentBusTopology ++
+    new freechips.rocketchip.system.BaseConfig)
+
+class MyGigaBoomConfig extends Config(
+  new chipyard.iobinders.WithUARTAdapter ++
+    new chipyard.iobinders.WithTieOffInterrupts ++
+    new chipyard.iobinders.WithBlackBoxSimMem ++
+    new chipyard.iobinders.WithTiedOffDebug ++
+    new chipyard.iobinders.WithSimSerial ++
+    new testchipip.WithTSI ++
+    new chipyard.config.WithBootROM ++
+    new chipyard.config.WithUART ++
+    new chipyard.config.WithL2TLBs(1024) ++
+    new freechips.rocketchip.subsystem.WithNoMMIOPort ++
+    new freechips.rocketchip.subsystem.WithNoSlavePort ++
+    new freechips.rocketchip.subsystem.WithInclusiveCache ++
+    new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++
+    new boom.common.WithMyGigaBooms ++
     new boom.common.WithNBoomCores(1) ++
     new freechips.rocketchip.subsystem.WithCoherentBusTopology ++
     new freechips.rocketchip.system.BaseConfig)
